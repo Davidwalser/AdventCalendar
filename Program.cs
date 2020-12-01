8a0469056326a76990f2f4e134d2d7cdbf0091ee
davidwalser <walser.dave@gmail.com>
2020-12-01 22:44:32 +0100

    init commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdventCalendar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Happy Advent!");
+        }
+    }
+}
