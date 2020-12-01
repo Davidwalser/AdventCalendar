7d0012dbf5139e1cb1204449968c97639ed0e86e
davidwalser <walser.dave@gmail.com>
2020-12-01 23:05:53 +0100

    added day1 puzzle2 + some refactoring

--- Day1/Day1.cs
+++ Day1/Day1.cs
@@ -4,15 +4,32 @@ namespace AdventCalendar.Day1
 {
     public class Day1
     {
-        public IEnumerable<(int, int)> GetNumber(int[] numbers)
+        public IEnumerable<(int, int)> Puzzle1(int[] numbers)
         {
-            for (int i = 0; i < numbers.Length; i++)
+            for (var i = 0; i < numbers.Length; i++)
             {
-                for (int j = i + 1; j < numbers.Length; j++)
+                for (var j = i + 1; j < numbers.Length; j++)
                 {
                     if (numbers[i] + numbers[j] == 2020)
                     {
                         yield return (numbers[i], numbers[j]);
+                    }
+                }
+            }
+        }
+
+        public IEnumerable<(int, int, int)> Puzzle2(int[] numbers)
+        {
+            for (var i = 0; i < numbers.Length; i++)
+            {
+                for (var j = i + 1; j < numbers.Length; j++)
+                {
+                    for (var k = 2; k < numbers.Length; k++)
+                    {
+                        if (numbers[i] + numbers[j] + numbers[k] == 2020)
+                        {
+                            yield return (numbers[i], numbers[j], numbers[k]);
+                        }
                     }
                 }
             }
--- Program.cs
+++ Program.cs
@@ -9,15 +9,26 @@ namespace AdventCalendar
         static void Main(string[] args)
         {
             Console.WriteLine("Happy Advent!");
+            Day1();
+            Console.ReadLine();
+        }
+
+        private static void Day1()
+        {
             var day1 = new Day1.Day1();
             var numbers = File.ReadAllLines(@"C:\Study\AdventCalendar\Day1\Numbers.txt").Select(int.Parse).ToArray();
-            var result = day1.GetNumber(numbers);
-            foreach (var (num1,num2) in result)
+            var puzzle1Result = day1.Puzzle1(numbers);
+            Console.WriteLine("Puzzle1:");
+            foreach (var (num1, num2) in puzzle1Result)
             {
-                Console.WriteLine($"Found numbers:{num1},{num2} result:{num1*num2}");
+                Console.WriteLine($"Found numbers:{num1},{num2} result:{num1 * num2}");
             }
-
-            Console.ReadLine();
+            Console.WriteLine("Puzzle2:");
+            var puzzle2Result = day1.Puzzle2(numbers);
+            foreach (var (num1, num2,num3) in puzzle2Result)
+            {
+                Console.WriteLine($"Found numbers:{num1},{num2},{num3} result:{num1 * num2 * num3}");
+            }
         }
     }
 }
