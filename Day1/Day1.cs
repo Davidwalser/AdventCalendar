7127620938c0cbb48c5d16c817948971a0a8ce7a
davidwalser <walser.dave@gmail.com>
2020-12-01 22:56:46 +0100

    Added day1 puzzle1

---
+++ Day1/Day1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace AdventCalendar.Day1
+{
+    public class Day1
+    {
+        public IEnumerable<(int, int)> GetNumber(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[i] + numbers[j] == 2020)
+                    {
+                        yield return (numbers[i], numbers[j]);
+                    }
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
 
 namespace AdventCalendar
 {
@@ -7,6 +9,15 @@ namespace AdventCalendar
         static void Main(string[] args)
         {
             Console.WriteLine("Happy Advent!");
+            var day1 = new Day1.Day1();
+            var numbers = File.ReadAllLines(@"C:\Study\AdventCalendar\Day1\Numbers.txt").Select(int.Parse).ToArray();
+            var result = day1.GetNumber(numbers);
+            foreach (var (num1,num2) in result)
+            {
+                Console.WriteLine($"Found numbers:{num1},{num2} result:{num1*num2}");
+            }
+
+            Console.ReadLine();
         }
     }
 }
